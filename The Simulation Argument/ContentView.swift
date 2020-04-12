49d46577c5a6c2bf755491f0bd032b1f4c0feb43
Saransh Sharma <saransh1337@gmail.com>
2020-04-12 23:15:42 +0530

    initial commit adds empty app

---
+++ The Simulation Argument/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  The Simulation Argument
+//
+//  Created by Saransh Sharma on 12/04/20.
+//  Copyright © 2020 saransh1337. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
